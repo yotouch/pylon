c9845494c6caabe8741ff5c913904f3dd9c93134
Weiming Yin <weiming.yin@gmail.com>
2016-04-19 17:59:02 +0800

    增加随机数的支持

---
+++ src/main/java/com/yotouch/base/web/util/RandUtil.java
@@ -0,0 +1,24 @@
+package com.yotouch.base.web.util;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class RandUtil {
+
+
+    public String genCode(int length) {
+
+        String s = "";
+        for (int i = 0; i < length; i ++) {
+            int r = (int)(Math.random()*9);
+            s += r;
+        }
+
+
+        return s;
+
+    }
+
+
+
+}
--- src/main/java/com/yotouch/core/ErrorCode.java
+++ src/main/java/com/yotouch/core/ErrorCode.java
@@ -5,5 +5,7 @@ public interface ErrorCode {
     int USER_START                  = 10000;
     int NO_SUCH_USER                = USER_START + 1;
     int LOGIN_FAILED_WRONG_PASSWORD = USER_START + 101;
+    int USER_EXISTS                 = USER_START + 102;
+    int VALID_SMS_CODE_VALID        = USER_START + 103;
 
 }
