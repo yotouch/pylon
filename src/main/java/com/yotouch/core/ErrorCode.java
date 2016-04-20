ca6bbde189b681368da850bbf80fc5e4b41cdc8f
Tammy <mengtingbi@163.com>
2016-04-20 10:17:26 +0800

    改错了，😄

--- src/main/java/com/yotouch/core/ErrorCode.java
+++ src/main/java/com/yotouch/core/ErrorCode.java
@@ -5,10 +5,10 @@ public interface ErrorCode {
     int USER_START                  = 10000;
     int NO_SUCH_USER                = USER_START + 1;
     int LOGIN_FAILED_WRONG_PASSWORD = USER_START + 101;
-    int INPUT_ERROR                 = USER_START + 201;
     int USER_EXISTS                 = USER_START + 102;
 
-    int INPUT_START                 = 20000;
-    int VALID_SMS_CODE_VALID        = INPUT_START + 103;
+    int VALID_SMS_CODE_VALID        = USER_START + 103;
 
+    int INPUT_START                 = 20000;
+    int INPUT_ERROR                 = INPUT_START + 201;
 }
