4f142cbcb61c295d38c9a5767bba221d46c269ea
Tammy <mengtingbi@163.com>
2016-04-20 10:03:24 +0800

    修改变量类型 输入的错误

--- src/main/java/com/yotouch/core/ErrorCode.java
+++ src/main/java/com/yotouch/core/ErrorCode.java
@@ -7,6 +7,8 @@ public interface ErrorCode {
     int LOGIN_FAILED_WRONG_PASSWORD = USER_START + 101;
     int INPUT_ERROR                 = USER_START + 201;
     int USER_EXISTS                 = USER_START + 102;
-    int VALID_SMS_CODE_VALID        = USER_START + 103;
+
+    int INPUT_START                 = 20000;
+    int VALID_SMS_CODE_VALID        = INPUT_START + 103;
 
 }
