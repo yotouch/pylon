f9703027dc90073c2cd83f7bb94d63d995943e7d
Weiming Yin <weiming.yin@gmail.com>
2016-07-06 15:31:16 +0800

    修改生成的二维码的 margin

--- src/main/java/com/yotouch/base/service/BarcodeServiceImpl.java
+++ src/main/java/com/yotouch/base/service/BarcodeServiceImpl.java
@@ -1,5 +1,11 @@
 package com.yotouch.base.service;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.EnumMap;
+import java.util.Map;
+
+
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.EncodeHintType;
 import com.google.zxing.Writer;
@@ -8,15 +14,8 @@ import com.google.zxing.client.j2se.MatrixToImageWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.QRCodeWriter;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 import org.springframework.stereotype.Service;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.EnumMap;
-import java.util.Map;
-
 
 @Service
 public class BarcodeServiceImpl implements BarcodeService {
@@ -28,13 +27,16 @@ public class BarcodeServiceImpl implements BarcodeService {
 
         Map<EncodeHintType, Object> hints = new EnumMap<>(EncodeHintType.class);
         hints.put(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.Q);
+        hints.put(EncodeHintType.CHARACTER_SET, "UTF-8");
+        hints.put(EncodeHintType.MARGIN, 1);
 
         BitMatrix matrix = writer.encode(
                 text,
                 BarcodeFormat.QR_CODE,
                 height,
                 height,
-                hints);
+                hints
+        );
 
         ByteArrayOutputStream bos = new ByteArrayOutputStream();
         MatrixToImageWriter.writeToStream(matrix, "png", bos);
