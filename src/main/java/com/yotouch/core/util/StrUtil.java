3d65841327a9ba4dcb4c8e102cf339e71195053a
Weiming Yin <weiming.yin@gmail.com>
2016-09-30 02:21:03 +0800

    增加生成vcode 的接口

--- src/main/java/com/yotouch/core/util/StrUtil.java
+++ src/main/java/com/yotouch/core/util/StrUtil.java
@@ -1,5 +1,6 @@
 package com.yotouch.core.util;
 
+import java.util.Random;
 import java.util.UUID;
 
 import org.springframework.stereotype.Component;
@@ -9,6 +10,24 @@ public class StrUtil {
     
     public String randStr() {
         return UUID.randomUUID().toString().toString().replace("-", "");
+    }
+
+    public String genVCode(int size) {
+        String s = "";
+        Random r = new Random();
+
+        while (size > 0) {
+            int i = Math.abs(r.nextInt());
+            s += i % 10;
+            size -= 1;
+        }
+
+        return s;
+    }
+
+    // for digital
+    public String genVcode() {
+        return this.genVCode(4);
     }
 
 }
