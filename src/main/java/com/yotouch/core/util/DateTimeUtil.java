049bc20325a5d1553061a5ec2801cd5123930a3e
Weiming Yin <weiming.yin@gmail.com>
2016-10-12 14:55:42 +0800

    Add datetime format

--- src/main/java/com/yotouch/core/util/DateTimeUtil.java
+++ src/main/java/com/yotouch/core/util/DateTimeUtil.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 public class DateTimeUtil {
@@ -18,6 +19,7 @@ public class DateTimeUtil {
     };
 
     static final SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
+    static final SimpleDateFormat sdfDateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     
     
     public static final Date parseString(String dt) {
@@ -33,6 +35,18 @@ public class DateTimeUtil {
 
     public static final String formatDate(Date d) {
         return sdfDate.format(d);
+    }
+
+    public static final String formatDate(Calendar cal) {
+        return formatDate(cal.getTime());
+    }
+
+    public static final String formatDateTime(Date d) {
+        return sdfDateTime.format(d);
+    }
+
+    public static final String formatDateTime(Calendar cal) {
+        return formatDateTime(cal.getTime());
     }
 
 
