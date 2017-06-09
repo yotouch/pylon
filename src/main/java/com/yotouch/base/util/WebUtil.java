2b9579875792844190885a3d5136c7bb493e5252
Tammy <mengtingbi@163.com>
2017-06-09 10:04:02 +0800

    获取访问者的ip地址

--- src/main/java/com/yotouch/base/util/WebUtil.java
+++ src/main/java/com/yotouch/base/util/WebUtil.java
@@ -313,4 +313,18 @@ public class WebUtil {
         return m;
     }
 
+    public String getIpAddr(HttpServletRequest request) {
+        String ip = request.getHeader("x-forwarded-for");
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getHeader("WL-Proxy-Client-IP");
+        }
+        if(ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+            ip = request.getRemoteAddr();
+        }
+        return ip;
+    }
+
 }
