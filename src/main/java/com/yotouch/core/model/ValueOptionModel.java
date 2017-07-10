742aa1d7dabbb6df6590c32928b8ef08442c239b
KingRiver <k@yotouch.com>
2017-07-10 15:46:01 +0800

    add three EntityModel

---
+++ src/main/java/com/yotouch/core/model/ValueOptionModel.java
@@ -0,0 +1,73 @@
+package com.yotouch.core.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by tammy on 09/07/2017.
+ */
+@Component
+public class ValueOptionModel extends EntityModel {
+    private String          displayName;
+    private String          name;
+    private String          pinyin;
+    private boolean         checked;
+    private Integer         weight;
+    private MetaEntityModel metaEntity;
+    private MetaFieldModel  metaField;
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPinyin() {
+        return pinyin;
+    }
+
+    public void setPinyin(String pinyin) {
+        this.pinyin = pinyin;
+    }
+
+    public boolean isChecked() {
+        return checked;
+    }
+
+    public void setChecked(boolean checked) {
+        this.checked = checked;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public MetaEntityModel getMetaEntity() {
+        return metaEntity;
+    }
+
+    public void setMetaEntity(MetaEntityModel metaEntity) {
+        this.metaEntity = metaEntity;
+    }
+
+    public MetaFieldModel getMetaField() {
+        return metaField;
+    }
+
+    public void setMetaField(MetaFieldModel metaField) {
+        this.metaField = metaField;
+    }
+}
