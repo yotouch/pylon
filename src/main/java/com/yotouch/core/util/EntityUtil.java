5bd3aa33e4c5a5b398f3bf470a385e5ae222991a
KingRiver <k@yotouch.com>
2017-07-28 14:17:26 +0800

    add @component for entityUtil

--- src/main/java/com/yotouch/core/util/EntityUtil.java
+++ src/main/java/com/yotouch/core/util/EntityUtil.java
@@ -3,9 +3,11 @@ package com.yotouch.core.util;
 import com.yotouch.core.entity.Entity;
 import com.yotouch.core.model.EntityModel;
 import com.yotouch.core.runtime.DbSession;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class EntityUtil {
     public static List<Entity> brList(DbSession dbSession, String childEntityName, String childSrFieldName, String fatherUuid) {
         return dbSession.queryRawSql(
