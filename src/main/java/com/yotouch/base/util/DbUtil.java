c71afdc571e2c8c4edd527ff75332edc2cc24874
King River <k@yotouch.com>
2017-11-12 02:19:59 +0800

    fix query in string

--- src/main/java/com/yotouch/base/util/DbUtil.java
+++ src/main/java/com/yotouch/base/util/DbUtil.java
@@ -4,46 +4,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @Component
 public class DbUtil {
-
-    public static QueryIn buildQueryIn(List<String> valueList) {
+    public static String buildQueryIn(List<String> valueList) {
         String[] qa = new String[valueList.size()];
         Arrays.fill(qa, "?");
-        String qaString = String.join(",", qa);
-        String valueString = String.join(",", valueList);
 
-        return new QueryIn(qaString, valueString);
-    }
-
-    public static class QueryIn {
-        private String query;
-        private String value;
-
-        public QueryIn() {
-        }
-
-        public QueryIn(String query, String value) {
-            this.query = query;
-            this.value = value;
-        }
-
-        public String getQuery() {
-            return query;
-        }
-
-        public void setQuery(String query) {
-            this.query = query;
-        }
-
-        public String getValue() {
-            return value;
-        }
-
-        public void setValue(String value) {
-            this.value = value;
-        }
+        return String.join(",", qa);
     }
 }
